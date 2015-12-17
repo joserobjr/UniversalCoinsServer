80465cd6bfd9b5862aaec00b7d67260fc68f990e
joserobjr <jose.rob.jr@gmail.com>
2015-12-17 01:52:47 -0300

    The packager block now drops its contents when it's broken

--- src/main/java/br/com/gamemods/universalcoinsserver/blocks/BlockPackager.java
+++ src/main/java/br/com/gamemods/universalcoinsserver/blocks/BlockPackager.java
@@ -3,14 +3,18 @@ package br.com.gamemods.universalcoinsserver.blocks;
 import br.com.gamemods.universalcoinsserver.GuiHandler;
 import br.com.gamemods.universalcoinsserver.UniversalCoinsServer;
 import br.com.gamemods.universalcoinsserver.tile.TilePackager;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.ContainerPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.world.World;
+
+import java.util.ArrayList;
 
 public class BlockPackager extends BlockRotary
 {
@@ -49,6 +53,31 @@ public class BlockPackager extends BlockRotary
         }
 
         return true;
+    }
+
+    @Override
+    public void breakBlock(World world, int x, int y, int z, Block block, int metadata)
+    {
+        TileEntity te = world.getTileEntity(x,y,z);
+        if(te instanceof TilePackager)
+        {
+            TilePackager packager = (TilePackager) te;
+            int size = packager.getSizeInventory();
+            ArrayList<ItemStack> drops = new ArrayList<>(size);
+            for(int i = 0; i < size; i++)
+            {
+                ItemStack stack = packager.getStackInSlot(i);
+                if(stack != null && stack.stackSize > 0)
+                {
+                    drops.add(stack);
+                    packager.setInventorySlotContents(i, null);
+                }
+            }
+
+            drop(world, x, y, z, drops);
+        }
+
+        super.breakBlock(world, x, y, z, block, metadata);
     }
 
     @Override
